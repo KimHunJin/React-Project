d5fa2dccf411101778db5846044abd2ee0974efe
HunJin Kim <ysg01129@naver.com>
2018-12-09 22:45:34 +0900

    [#18] bug. login link 링크하는 방법을 잘 모르겠다..

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,18 +3,28 @@ import * as React from 'react';
 import {Component} from 'react';
 import {MainPage} from "app/containers/Main";
 import {TopNavigation} from "app/components";
+// import { createBrowserHistory } from 'history';
+// import { Route, Router, Switch } from 'react-router'
+// import { LoginPage } from './containers/Login';
 
 // render react DOM
 
 export class App extends Component {
 
-
-
     render(): React.ReactNode {
+
+        // const history = createBrowserHistory();
+
         return (
             <div data-reactroot>
                 <TopNavigation/>
-                <MainPage/>
+                {/* <Router history={history}>
+                    <Switch> */}
+                    <MainPage/>
+                        {/* <Route path="/" components={MainPage}/>
+                        <Route path="/login" components={LoginPage}/> */}
+                    {/* </Switch> */}
+                {/* </Router> */}
             </div>
         )
     }
