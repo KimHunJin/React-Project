4af0c2477264f83ca45e14dfd555558d3f4378a0
KimHunJin <khj1115@midasit.com>
2018-12-11 18:11:05 +0900

    [#22] feat. favorite rerender

--- src/app/components/Feed/FeedItem/ItemListUserInfo/index.tsx
+++ src/app/components/Feed/FeedItem/ItemListUserInfo/index.tsx
@@ -4,6 +4,7 @@ import {FeedModel} from "app/model/FeedModel/index";
 import {observer} from "mobx-react";
 import userStore from "app/stores/UserStore";
 import APIConn from "../../../../../lib/http/service_util";
+import feedStore from "app/stores/FeedStore";
 
 export interface FeedProps {
     feed: FeedModel
@@ -15,8 +16,11 @@ export class ItemListUserInfo extends React.Component<FeedProps> {
     handlerFeedLike(feed: FeedModel) {
         if(userStore.userModel) {
             APIConn.getInstance().postFavoriteArticle(true, feed.slug).then(res => {
-                if(res.statue == 200) {
+                console.log(res);
+                if(res.status == 200) {
                     feed.favorited = true;
+                    console.log('test')
+                    feedStore.fetchArticleData()
                 }
             })
         } else {
